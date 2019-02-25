2f9a084b901e90e83497f3b15f7e6c29a87e87f2
evbot_ <45015207+evbot-15@users.noreply.github.com>
2019-02-24 21:23:55 -0600

    Update index template using render shortcut

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,12 +6,19 @@ from django.template import loader
 
 
 def index(request):
+    # using the HttpResponse() method
+    """
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+    """
+    # using the render() shortcut, we don't need to import HttpResponse
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
