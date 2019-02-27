66121f17da23b82e850b494b7689d51f09ed78be
evbot_ <45015207+evbot-15@users.noreply.github.com>
2019-02-27 13:31:31 -0600

    End tutorial 5, add tests

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -21,6 +21,12 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 # Results view using generic views system
 class ResultsView(generic.DetailView):
     model = Question
